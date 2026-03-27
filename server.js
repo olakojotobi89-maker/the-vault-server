01419394299b7cd0dee83b0741f95596b163424d
Olakojo Tobi <olakojotobi89@gmail.com>
2026-03-27 16:19:31 +0100

    surgical update to mongo srv

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// --- MONGODB CONNECTION (LEGACY STABLE VERSION) ---
-// This version is much harder for mobile networks to block
-const MONGO_URI = process.env.MONGO_URI || "mongodb://olakojotobi89_db_user:VaultPass2026@cluster0-shard-00-00.fuesl9b.mongodb.net:27017,cluster0-shard-00-01.fuesl9b.mongodb.net:27017,cluster0-shard-00-02.fuesl9b.mongodb.net:27017/vaultDB?ssl=true&replicaSet=atlas-fuesl9b-shard-0&authSource=admin&retryWrites=true&w=majority";
+// --- MONGODB CONNECTION ---
+// Updated to modern SRV string for better Render compatibility
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://olakojotobi89_db_user:VaultPass2026@cluster0.fuesl9b.mongodb.net/vaultDB?retryWrites=true&w=majority";
 
 mongoose.connect(MONGO_URI, {
     serverSelectionTimeoutMS: 10000 // Giving it 10 seconds for mobile data lag
